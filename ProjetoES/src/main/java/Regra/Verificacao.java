5ebc41c0a30fbb8ed31734d256e16a5531ed1704
tmots <tmots@iscte-iul.pt>
2021-05-06 17:03:30 +0100

    alterações na regra para integrar com o demais do projeto, juntar saver e verificador

---
+++ ProjetoES/src/main/java/Regra/Verificacao.java
@@ -0,0 +1,26 @@
+package Regra;
+
+import java.util.ArrayList;
+
+public class Verificacao { //incompleto
+/*	boolean result=false;
+	ArrayList<Integer> leituras=new ArrayList<>();
+	public boolean check(int limite, int contagem){
+		return limite<=contagem;
+	}
+
+	public boolean Verify(Regras r, ArrayList<Integer> leituras){
+    	if (r.lop==0) //or
+    		result=false;
+    	else // and
+    		result=true;
+    	for(AuxRegra condition: r.metrics)
+			for(int resultado: leituras)
+				//falta verificar se tipo do limite corresponde ao tipo da leitura
+				//seja pelo nome ou pelo id [1,5]
+   		 		if (check(condition.getLimite(), resultado)!=result)
+    				return !result;
+    return result;
+
+    }
+*/}
