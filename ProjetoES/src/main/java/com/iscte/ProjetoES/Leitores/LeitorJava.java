fe9c05d6ccd3572bc7e1261212f10f2ea7261611
MyNameIsKellKell <jgafa@iscte-iul.pt>
2021-05-09 15:33:13 +0100

    Update 2 ficheiros

--- ProjetoES/src/main/java/com/iscte/ProjetoES/Leitores/LeitorJava.java
+++ ProjetoES/src/main/java/com/iscte/ProjetoES/Leitores/LeitorJava.java
@@ -15,8 +15,10 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.table.AbstractTableModel;
 
 import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 
 import com.iscte.ProjetoES.Metodo;
 import com.iscte.ProjetoES.Escritores.EscritorExcel;
@@ -31,6 +33,9 @@ public class LeitorJava extends AbstractTableModel {
 	private String name;
 	private File file;
 	private Class cls;
+
+	private LeitorJava() {
+	}
 
 	public void abrirJava() {
 		Workbook workbook;
@@ -59,65 +64,15 @@ public class LeitorJava extends AbstractTableModel {
 	public File escolherJava() {
 		JFileChooser escolherJava = new JFileChooser();
 		escolherJava.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-		
-		int opcaoEscolhida = escolherJava.showOpenDialog(null);
-        if(opcaoEscolhida == escolherJava.APPROVE_OPTION){
-           file = escolherJava.getSelectedFile();
-           filepath =  (String) file.getAbsolutePath();
-           name = (String) file.getAbsolutePath().substring(file.getAbsolutePath().lastIndexOf("\\")+1);
-          
-           listAllFiles(file);
-        }
 
-		System.out.println("List of files and directories in the specified directory:" + file.getName());
-		
-		return file;
-
-		int opcaoEscolhida = escolherJava.showOpenDialog(null);
-		if (opcaoEscolhida == escolherJava.APPROVE_OPTION) {
+		int option = escolherJava.showOpenDialog(null);
+		if (option == escolherJava.APPROVE_OPTION) {
 			file = escolherJava.getSelectedFile();
 			filepath = (String) file.getAbsolutePath();
 			name = (String) file.getAbsolutePath().substring(file.getAbsolutePath().lastIndexOf("\\") + 1);
 
-	}
-
-	private void lerFicheiro(File file) {
-			if (file.getName().endsWith(".java")) {
-				try {
-					/*Class cls = Class.forName(Classe);
-					NOM_class= cls.getDeclaredMethods().length;*/
-					int count = 0;
-					Scanner myReader = new Scanner(file);
-					while (myReader.hasNextLine()) {
-						count++;
-						String data = myReader.nextLine();
-						LOC_class++;
-						String[] words = data.split(" ");
-						Classe = file.getName().replace(".java","");
-						if (count == 1 && words.length > 0) {
-							if (words[0] == "package") {
-								Package = words[1];
-							}
-						}
-
 			listAllFiles(file);
 		}
-
-						
-						
-						if(procurarCYCLO("for",data) || procurarCYCLO("if",data) || procurarCYCLO("case",data) || procurarCYCLO("while",data)) {
-							CYCLO_method++;
-						}
-						
-						
-						
-						if (data.isEmpty()) {
-//							if (data.isBlank()) {
-							LOC_class--;
-						}
-						Metodo met = new Metodo(MethodID, Package, Classe, method, NOM_class, LOC_class, WMC_class,
-								LOC_method, CYCLO_method);
-						EscritorExcel.adicionaLista(met);
 
 		System.out.println("List of files and directories in the specified directory:" + file.getName());
 
@@ -187,13 +142,6 @@ public class LeitorJava extends AbstractTableModel {
 					e.printStackTrace();
 				}
 
-
-				/*
-				 * System.out.println("File name: "+file.getName());
-				 * System.out.println("File path: "+file.getAbsolutePath());
-				 * System.out.println("Size :"+file.getTotalSpace()); System.out.println(" ");
-				 */
-			}
 				System.out.println("File name: " + met.getClasse());
 
 			}
